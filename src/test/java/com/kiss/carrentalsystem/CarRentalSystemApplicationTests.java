ca9a195f163445c2e77a3fb1f50c501958c6a1e0
Clevercael1 <caeloflaherty@gmail.com>
2023-10-26 09:08:18 +0100

    Fixing github files

---
+++ src/test/java/com/kiss/carrentalsystem/CarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiss.carrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
