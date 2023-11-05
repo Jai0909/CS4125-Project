6f024a8dca05f633701b70ffd90384266b00d303
Dennis <20250762@studentmail.ul.ie>
2023-11-05 14:44:56 +0000

    Initial Commit

--- src/main/java/com/kiss/carrentalsystem/entity/Car.java
+++ src/main/java/com/kiss/carrentalsystem/entity/Car.java
@@ -3,10 +3,10 @@ import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.*;
 @Entity
-@Table(name="car")
+@Table(name="car") //todo check table name
 public class Car {
     @Id
-    @Column(name="makeAndModel", length = 255)
+    @Column(name="makeAndModel", length = 255) //todo adjust names and lengths to fit
     private String makeAndModel;
     @Column(name="lisencePlate", length = 255)
     private String lisencePlate;
@@ -21,5 +21,62 @@ public class Car {
 
 
     public Car() {
+    }
+
+    public Car(String makeAndModel, String lisencePlate, int milage, String vehicleType, boolean damaged, float basePrice) {
+        this.makeAndModel = makeAndModel;
+        this.lisencePlate = lisencePlate;
+        this.milage = milage;
+        this.vehicleType = vehicleType;
+        this.damaged = damaged;
+        this.basePrice = basePrice;
+    }
+
+    public String getMakeAndModel() {
+        return makeAndModel;
+    }
+
+    public void setMakeAndModel(String makeAndModel) {
+        this.makeAndModel = makeAndModel;
+    }
+
+    public String getLisencePlate() {
+        return lisencePlate;
+    }
+
+    public void setLisencePlate(String lisencePlate) {
+        this.lisencePlate = lisencePlate;
+    }
+
+    public int getMilage() {
+        return milage;
+    }
+
+    public void setMilage(int milage) {
+        this.milage = milage;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public boolean isDamaged() {
+        return damaged;
+    }
+
+    public void setDamaged(boolean damaged) {
+        this.damaged = damaged;
+    }
+
+    public float getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(float basePrice) {
+        this.basePrice = basePrice;
     }
 }
