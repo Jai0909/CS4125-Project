7bf1444c72e6f4fecac9b8028c1252fd3e04258e
Dennis <20250762@studentmail.ul.ie>
2023-11-06 12:54:38 +0000

    initial commit
    Added validator methods for user

--- src/main/java/com/kiss/carrentalsystem/validator/UserValidator.java
+++ src/main/java/com/kiss/carrentalsystem/validator/UserValidator.java
@@ -1,4 +1,35 @@
 package com.kiss.carrentalsystem.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidator {
+    public boolean validateName(String user) {
+        if (containsNumbers(user)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validatePassword(String password) {
+        if (!containsNumbers(password) || !containsCapitalLetters(password) || password.length() < 8) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean containsNumbers(String input) {
+        Pattern pattern = Pattern.compile(".*\\d.*");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    private static boolean containsCapitalLetters(String input) {
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
