e0dd1c19ceecd0a24beaf186a80fee0c8f9ea16d
Clevercael1 <caeloflaherty@gmail.com>
2023-11-06 22:33:34 +0000

    Fixing car datafields and adding DTOs

--- src/main/java/com/kiss/carrentalsystem/CarRentalSystemApplication.java
+++ src/main/java/com/kiss/carrentalsystem/CarRentalSystemApplication.java
@@ -1,4 +1,4 @@
-package com.kiss.carrentalsystem.misc;
+package com.kiss.carrentalsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/kiss/carrentalsystem/dto/BookingDTO.java
+++ src/main/java/com/kiss/carrentalsystem/dto/BookingDTO.java
@@ -1,4 +1,56 @@
 package com.kiss.carrentalsystem.dto;
 
+import com.kiss.carrentalsystem.entity.Car;
+import com.kiss.carrentalsystem.entity.User;
+import jakarta.persistence.Column;
+
+import java.util.Date;
+
 public class BookingDTO {
+    private Car car;
+    private User user;
+    private Date bookingStartDate;
+    private Date bookingEndDate;
+
+    public BookingDTO() {
+    }
+
+    public BookingDTO(Car car, User user, Date bookingStartDate, Date bookingEndDate) {
+        this.car = car;
+        this.user = user;
+        this.bookingStartDate = bookingStartDate;
+        this.bookingEndDate = bookingEndDate;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getBookingStartDate() {
+        return bookingStartDate;
+    }
+
+    public void setBookingStartDate(Date bookingStartDate) {
+        this.bookingStartDate = bookingStartDate;
+    }
+
+    public Date getBookingEndDate() {
+        return bookingEndDate;
+    }
+
+    public void setBookingEndDate(Date bookingEndDate) {
+        this.bookingEndDate = bookingEndDate;
+    }
 }
