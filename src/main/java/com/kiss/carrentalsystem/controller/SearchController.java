42821b9c4cf532a7b4b86a7943d1ef138964315f
Clevercael1 <caeloflaherty@gmail.com>
2023-11-24 01:59:40 +0000

    Fixed search method, need to fix return type still

--- src/main/java/com/kiss/carrentalsystem/controller/SearchController.java
+++ src/main/java/com/kiss/carrentalsystem/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.kiss.carrentalsystem.controller;
 import com.kiss.carrentalsystem.response.DefaultResponse;
 import com.kiss.carrentalsystem.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +14,23 @@ public class SearchController {
 
     @Autowired
     private SearchService searchService;
-    @GetMapping("/search")
-    public ResponseEntity<?> search(@RequestParam String toSearch) {
-        DefaultResponse searchResponse = (DefaultResponse) searchService.search(toSearch);
-        return ResponseEntity.ok(searchResponse);
-    }
 
+    @GetMapping(
+            path = "/search/{toSearch}",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE
+            }
+    )
+    public ResponseEntity<?> search(@PathVariable String toSearch) {
+        DefaultResponse searchResponse;
+        if (!searchService.search(toSearch).isEmpty()) {
+            searchResponse = new DefaultResponse("A result was found", true);
+        } else {
+            searchResponse = new DefaultResponse("No results", false); //todo change response type to include an array to return array
+        }
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(searchResponse);
+    }
 }
